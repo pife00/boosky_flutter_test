9c873c3d8c2f634bdfb96be34e09d3e24e0db9a4
pife00 <pife00@gmail.com>
2023-05-12 22:40:41 -0500

    se debe instalar mas cosas mirar firebase official page

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,14 @@ import 'package:boosky/state/book_shelf.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
